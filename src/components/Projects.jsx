4fc65abb5145a1d1a9b15f2283987df5b35f4ebf
Purity Turunen <k7jepu00@students.oamk.fi>
2025-08-08 16:48:50 +0300

    Added projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,41 +1,68 @@
-import React from 'react';
-import Little from '../assets/projects/Lemon.JPG';
-import elegantShoes from '../assets/projects/elegantShoes.JPG';
-import Robotics from '../assets/projects/LabInventory.JPG';
-import tastyEats from '../assets/projects/tastyEats.JPG';
-import crypto from '../assets/projects/crypto.JPG';
-import Footer from './Footer';
+import React from "react";
+import Little from "../assets/projects/Lemon.JPG";
+import elegantShoes from "../assets/projects/elegantShoes.JPG";
+import tastyEats from "../assets/projects/tastyEats.JPG";
+import crypto from "../assets/projects/crypto.JPG";
+import Robotics from "../assets/projects/Lab.png"; // Use the correct image path you want
+import Lab from "../assets/projects/Lab.png"; // Looks like same as Robotics? Remove if duplicate
+import Footer from "./Footer";
 
 const Projects = () => {
   return (
     <div
       name="projects"
-      className="w-full md:h-screen text-gray-300 bg-[#0a192f] "
+      className="w-full md:h-screen text-gray-300 bg-[#0a192f]"
     >
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-        <div className="pb-6 mt-20 ">
-          {/* <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
-            Projects
-          </p> */}
-
+        <div className="pb-6 mt-20">
           <p
             style={{ zIndex: 50, visibility: "visible", position: "relative" }}
             className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600"
           >
             Projects
           </p>
-
           <p className="py-4 text-xl">Check out some of my school projects:</p>
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-6 mb-2">
           <div
-            style={{ backgroundImage: `url(${elegantShoes})` }}
-            className=" h-[180px] shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div"
+            style={{ backgroundImage: `url(${Lab})` }}
+            className="h-[180px] shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div"
           >
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font bold text-white tracking-wider">
-                ReacJS ElegantShoes Application
+              <span className="text-2xl font-bold text-white tracking-wider">
+                University of Oulu: Robotics Lab Inventory System
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  href="https://updated-inventory.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a
+                  href="https://github.com/TurunenP/Updated-Inventory"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Github
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div
+            style={{ backgroundImage: `url(${elegantShoes})` }}
+            className="h-[180px] shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div"
+          >
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                ReactJS ElegantShoes Application
               </span>
               <div className="pt-8 text-center">
                 <a
@@ -62,10 +89,10 @@ const Projects = () => {
 
           <div
             style={{ backgroundImage: `url(${crypto})` }}
-            className=" h-[180px] shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div"
+            className="h-[180px] shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div"
           >
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font bold text-white tracking-wider">
+              <span className="text-2xl font-bold text-white tracking-wider">
                 Crypto Application
               </span>
               <div className="pt-8 text-center">
@@ -93,19 +120,27 @@ const Projects = () => {
 
           <div
             style={{ backgroundImage: `url(${tastyEats})` }}
-            className=" h-[180px] shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div"
+            className="h-[180px] shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div"
           >
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font bold text-white tracking-wider">
+              <span className="text-2xl font-bold text-white tracking-wider">
                 TastyEatsRecipe Application
               </span>
               <div className="pt-8 text-center">
-                <a href="https://react-tasty-eats.vercel.app/">
+                <a
+                  href="https://react-tasty-eats.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
                 </a>
-                <a href="https://github.com/TurunenP/ReactTastyEats">
+                <a
+                  href="https://github.com/TurunenP/ReactTastyEats"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Github
                   </button>
@@ -116,69 +151,24 @@ const Projects = () => {
 
           <div
             style={{ backgroundImage: `url(${Little})` }}
-            className=" h-[180px] shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div"
+            className="h-[180px] shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div"
           >
             <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font bold text-white tracking-wider">
-                Coursera_Little-Lemon-restaurant:(UX/UI)
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Coursera Little Lemon Restaurant (UX/UI)
               </span>
               <div className="pt-8 text-center">
-                {/* <a href='/'>
-        <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>OnGoing</button>
-              </a> */}
-                <a href="https://github.com/TurunenP/Coursera_Little-Lemon-restaurant">
+                <a
+                  href="https://github.com/TurunenP/Coursera_Little-Lemon-restaurant"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Github
                   </button>
                 </a>
               </div>
             </div>
-          </div>
-
-          <div
-            style={{ backgroundImage: `url(${Robotics})` }}
-            className=" h-[180px] shadow-lg shadow-[#040c16] group container rounded-md flex justify-center text-center items-center mx-auto content-div"
-          >
-            <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font bold text-white tracking-wider">
-                <p className="text-[18px]">
-                  University of Oulu: Robotics Lab Inventory System
-                  <p className="text-yellow-500">(OnGoing)</p>
-                </p>
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://robo-lab-system.netlify.app/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="https://github.com/TurunenP/Updated-Inventory">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Github
-                  </button>
-                </a>
-              </div>
-            </div>
-
-            {/* <div className="opacity-0 group-hover:opacity-100">
-              <span className="text-2xl font bold text-white tracking-wider">
-                <p className="text-[18px]">
-                  University of Oulu: Robotics Lab Inventory System
-                </p>
-              </span>
-              <div className="pt-8 text-center">
-                <a href="https://robo-lend-lab-equipment-inventory-system.vercel.app/">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Demo
-                  </button>
-                </a>
-                <a href="https://github.com/RoboLend-Lab-Equipment-Inventory-System/RoboLend-Lab-Equipment-Inventory-System.git">
-                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
-                    Github
-                  </button>
-                </a>
-              </div>
-            </div> */}
           </div>
         </div>
       </div>
